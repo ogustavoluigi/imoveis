e7aa9cd935244f9ce78fefb40f6356af053eb90e
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-25 16:31:32 -0300

    ok

--- content/index.blade.php
+++ content/index.blade.php
@@ -4,9 +4,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Agencia de Viagem</title>
 </head>
+<header>
+    <h1>SUA MELHOR OPÇÃO DE VIAGEM ESTA AQUI</h1>
+</header>
 <body>
-    
+<nav>
+  <ul class="menu">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Sobre</a></li>
+	  		<li><a href="#">Escolha sua melhor experiência</a>
+	         	<ul>
+	                  <li><a href="#">Viagem Terreste</a></li>
+	                  <li><a href="#">Viagem Aérea</a></li>
+	       		</ul>
+    </ul>          
+</nav>
 </body>
 </html>
