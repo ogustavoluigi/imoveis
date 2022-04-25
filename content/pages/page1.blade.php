8a2214dc493ebe3b0a579ce8efa113f741d06a7b
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-25 16:13:36 -0300

    confirmar

--- content/pages/page1.blade.php
+++ content/pages/page1.blade.php
@@ -4,9 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page1</title>
+    <title>ViaTURISMO</title>
 </head>
+<header>
+    <h1>SUA MELHOR OPÇÃO DE VIAGEM</h1>
+    
+</header>
 <body>
-    <h1>Page1</h1>
+    <h1>Via Turismo</h1>
+
 </body>
 </html>
