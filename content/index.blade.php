1e7879b6b7867c6d1f1fec6311fed6802eec0bea
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-26 08:42:51 -0300

    ok

--- content/index.blade.php
+++ content/index.blade.php
@@ -11,16 +11,6 @@
     <h1>SUA MELHOR OPÇÃO DE VIAGEM ESTA AQUI</h1>
 </header>
 <body>
-<nav>
-  <ul class="menu">
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Sobre</a></li>
-	  		<li><a href="#">Escolha sua melhor experiência</a>
-	         	<ul>
-	                  <li><a href="#">Viagem Terreste</a></li>
-	                  <li><a href="#">Viagem Aérea</a></li>
-	       		</ul>
-    </ul>          
-</nav>
+
 </body>
 </html>
