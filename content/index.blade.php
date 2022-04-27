ea7035ca6243e5169f2ae3cadfd9a9410cdf31f2
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-27 10:11:09 -0300

    ok

--- content/index.blade.php
+++ content/index.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link ref="stylesheet" href="reset.css">  
-    <link rel="stylesheet" href="styles.css">
+    <link ref="stylesheet" href="{{ asset('template/css/reset.css') }}">  
+    <link rel="stylesheet" href="{{ asset('template/css/styles.css') }}">
     <title>Agencia de Viagem</title>
 </head>
 <header>
