791b396f16d091c201b965b4086030ce8ce2a093
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-29 09:22:01 -0300

    ok

--- content/master.blade.php
+++ content/master.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> 
     <link ref="stylesheet" href="{{ asset('template/css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('template/css/master.css') }}">
     <title>@yield('title')</title>
@@ -16,14 +17,15 @@
         <div class="container-logo">
             <div class="logo-image"></div>
             <div class="logo-text"></div>
-            <h1>EMBARQUE NESTA EXPERIÊNCIA<h1>
+            <h1>WEB IMÓVEIS <h1>
         </div>
         <div class="cabecalho'">
             <ul>
                 <li><a href="#">Home</a></li>
-                <li><a href="#">Pacotes</a></li>
-                <li><a href="#">Aereo</a></li>
-                <li><a href="#">Rodoviario</a></li>
+                <li><a href="#">Casas</a></li>
+                <li><a href="#">Apartamentos</a></li>
+                <li><a href="#">Terrenos</a></li>
+                <li><a href="#">Salas comerciais</a></li>
             </ul>
         </div>
     </header>
--- content/pages/page1.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('template.'.config('settings.template').'.content.master')
-@section('title','Venha Conosco')
-@section('content')
-<p>Viva esta experiencia<p>
-    
-@stop
