37e9252e4957ed7c48e0ccece6aeb52c5b68736f
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-04-29 10:05:44 -0300

    ok

--- content/master.blade.php
+++ content/master.blade.php
@@ -13,14 +13,11 @@
 </head>
 
 <body>
-    <header>
-        <div class="container-logo">
-            <div class="logo-image"></div>
-            <div class="logo-text"></div>
-            <h1>WEB IMÓVEIS <h1>
-        </div>
-        <div class="cabecalho'">
-            <ul>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">WEB IMÓVEL</a>
+                <ul>
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Casas</a></li>
                 <li><a href="#">Apartamentos</a></li>
@@ -28,6 +25,15 @@
                 <li><a href="#">Salas comerciais</a></li>
             </ul>
         </div>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    <header>
+      
+      
     </header>
     @yield('content')
 
