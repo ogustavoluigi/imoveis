601b83db4c90a0c01a5b3671b4a3d59a03cf350c
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-05-01 15:03:09 -0300

    ok

--- content/master.blade.php
+++ content/master.blade.php
@@ -8,8 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="shortcut icon" href="{{ storage('images/nave.ico')}}" type="image/x-icon" />
     <link ref="stylesheet" href="{{ storage('/css/reset.css') }}">
+    <link rel="stylesheet" href="{{ storage('/css/index.css') }}">
     <link rel="stylesheet" href="{{ storage('/css/master.css') }}">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
     <title>@yield('title')</title>
     @yield('head')
