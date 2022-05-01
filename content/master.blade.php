dd22ac5d3f98816197417f7e5efd3789e486decc
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-05-01 11:21:35 -0300

    ok

--- content/master.blade.php
+++ content/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="shortcut icon" href="{{ storage('images/logo.ico')}}" type="image/x-icon" />
+    <link rel="shortcut icon" href="{{ storage('images/nave.ico')}}" type="image/x-icon" />
     <link ref="stylesheet" href="{{ storage('/css/reset.css') }}">
     <link rel="stylesheet" href="{{ storage('/css/master.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
@@ -19,9 +19,9 @@
     <header>
         <nav class="navbar navbar-light bg-light">
             <div class="container-fluid">
-                <img src="{{ storage('images/nave.ico')}}" alt="WEB IMOVEL">
+                <img src="{{ storage('images/nave2.png')}}" alt="WEB IMOVEL">
                 <form class="d-flex">
-                    <input class="form-control me-2" type="search" placeholder="Encotre seu imóvel aqui" aria-label="Search">
+                    <input class="form-control me-2" type="search" placeholder="Encotre seu imóvel" aria-label="Search">
                     <button class="btn btn-outline-success" type="submit">Pesquisar</button>
                 </form>
             </div>
@@ -30,7 +30,7 @@
     @yield('content')
 
     <footer>
-        <p>Texto <a href="#">entre em contato por Email</a></p>
+        <p>Estamos ansiosos para realizar seu sonho <a href="#"  ></a></p>
     </footer>
 
 </body>
