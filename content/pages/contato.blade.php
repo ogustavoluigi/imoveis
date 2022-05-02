8eaf64b422d20a8670d309d58f8720856811d356
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-05-02 15:40:48 -0300

    ok

--- content/pages/contato.blade.php
+++ content/pages/contato.blade.php
@@ -1,31 +1,17 @@
 @extends('template.'.config('settings.template').'.content.master')
-@section('title','Home')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-<body>
-    <h1>Buscando os melhores Imóveis?</h1>
-    <h2>São 20 anos de experiência de mercado</h2>
-    <p>Entre em contato que estamos a </p>
-<div class="form">
-  <label for="formControlInput1" class="form-label">Nome </label>
-  <input type="text" class="form-control" id="formControlInput1" placeholder="Nome completo">
+@section('title','Contato')
+@section('content')
+<h2>Buscando os melhores Imóveis?</h2>
+<h3>São 20 anos de experiência de mercado</h3>
+<p>Entre em contato que temos uma equipe espcializada em melhores neg</p>
+<form>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
 </div>
-<div class="form">
-  <label for="formControlInput1" class="form-label">Telefone</label>
-  <input type="text" class="form-control" id="formControlInput1" placeholder="Seu melhor numero">
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
 </div>
-<div class="form">
-  <label for="formControlInput1" class="form-label">E-mail</label>
-  <input type="email" class="form-control" id="formControlInput1" placeholder="Seu melhor e-mail">
-</div>
-<div class="form">
-  <label for="formControlTextarea1" class="form-label">Descreva o tipo de imóvel que você procura</label>
-  <textarea class="form-control" id="formControlTextarea1" rows="12"></textarea>
-</div> 
-</body>
-</html>
+</form>
+@stop
--- content/pages/somos.blade.php
+++ content/pages/somos.blade.php
@@ -1,8 +1,12 @@
 @extends('template.'.config('settings.template').'.content.master')
 @section('title','Home')
 @section('content')
-<p>Como foi pensado a Web Imóveis, em um unico lugar, você tem as opções de imóveis anunciados em diversas plataformas de venda, assim como o seu anuncio também.<p>
-<p>Um lugar para sua venda e sua compra de Imóveis na web<p>
-<p>Viva a experiencia de conhecer as opçções de compra e venda em um unico portal<p>
-    
-@stop
+<p>Como foi pensado a Web Imóveis, em um unico lugar, você tem as opções de imóveis anunciados em diversas plataformas
+    de venda, assim como o seu anuncio também.
+<p>
+<p>Um lugar para sua venda e sua compra de Imóveis na web
+<p>
+<p>Viva a experiencia de conhecer as opçções de compra e venda em um unico portal
+<p>
+
+    @stop
