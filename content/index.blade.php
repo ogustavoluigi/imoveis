7d5a023554281e2133e7cffff9f5355b06546fcf
vanessarocha <vanessa.cardoso@rede.ulbra.br>
2022-05-04 12:03:08 -0300

    ok

--- content/index.blade.php
+++ content/index.blade.php
@@ -3,62 +3,70 @@
 @section('content')
 <div class="container" id="mini-banners">
     <div class="row justify-content-around">
-        <div class="col-12 primery-bg-color" id="mini-banner-1">
-            <h2 class="primery-bg-color">Casa de Campo</h2>
-            <img class="img-fluid rounded" src="{{ storage('images/casa_de_campo.jpg')}}" alt="casadecampo">
+        <h2 class="primery-bg-color">Casa de Campo</h2>
+        <div class=" col-md-3 m-3 primery-bg-color" id="mini-banner-1">
+            <img class=" img-fluid rounded" src="{{ storage('images/casa_de_campo.jpg')}}" alt="casadecampo">
             <br>
             <a href="#">Saiba Mais</a>
-            <br>
+        </div>
+        <br>
+        <div class=" col-md-3 m-3 primery-bg-color" id="mini-banner-2">
             <img class="img-fluid rounded" src="{{ storage('images/cdc2.jpg')}}" alt="casadecampo1">
             <br>
             <a href="#">Saiba Mais</a>
-            <br>
+        </div>
+        <br>
+        <div class=" col-md-3 m-3 primery-bg-color" id="mini-banner-3">
             <img class="img-fluid rounded" src="{{ storage('images/cdc3.jpg')}}" alt="casadecampo2">
             <br>
             <a href="#">Saiba Mais</a>
-            <br>
+        </div>
+        <br>
+        <div class=" col-md-3 m-3 primery-bg-color" id="mini-banner-4">
             <img class="img-fluid rounded" src="{{ storage('images/cdc4.jpg')}}" alt="casadecampo3">
             <br>
             <a href="#">Saiba Mais</a>
+        </div>
+        <div class="col-12 primery-bg-color" id="mini-banner">
+
+            <h2 class="primery-bg-color">Casa de Praia</h2>
+            <img class="img-fluid rounded" src="{{ storage('images/casa_de_praia.jpg')}}" alt="casadepraia">
+            <br>
+            <a href="#">Saiba Mais</a>
+            <br>
+            <img class="img-fluid rounded" src="{{ storage('images/cdp2.jpg')}}" alt="casadepraia">
+            <br>
+            <a href="#">Saiba Mais</a>
+            <br>
+            <img class="img-fluid rounded" src="{{ storage('images/cdp3.jpg')}}" alt="casadepraia">
+            <br>
+            <a href="#">Saiba Mais</a>
+            <br>
+            <img class="img-fluid rounded" src="{{ storage('images/cdp4.jpg')}}" alt="casadepraia">
+            <br>
+            <a href="#">Saiba Mais</a>
             <div class="col-12 primery-bg-color" id="mini-banner-3">
-                <h2 class="primery-bg-color">Casa de Praia</h2>
-                <img class="img-fluid rounded" src="{{ storage('images/casa_de_praia.jpg')}}" alt="casadepraia">
+                <h2 class="primery-bg-color">Casa na Cidade</h2>
+                <img class="img-fluid rounded" src="{{ storage('images/casa_de_cidade.jpg')}}" alt="casadecidade">
                 <br>
                 <a href="#">Saiba Mais</a>
                 <br>
-                <img class="img-fluid rounded" src="{{ storage('images/cdp2.jpg')}}" alt="casadepraia">
+                <img class="img-fluid rounded" src="{{ storage('images/casadecidad2.jpg')}}" alt="casadecidade">
                 <br>
                 <a href="#">Saiba Mais</a>
                 <br>
-                <img class="img-fluid rounded" src="{{ storage('images/cdp3.jpg')}}" alt="casadepraia">
+                <img class="img-fluid rounded" src="{{ storage('images/casadecidad3.jpg')}}" alt="casadecidade">
                 <br>
                 <a href="#">Saiba Mais</a>
                 <br>
-                <img class="img-fluid rounded" src="{{ storage('images/cdp4.jpg')}}" alt="casadepraia">
+                <img class="img-fluid rounded" src="{{ storage('images/casadecid4.jpg')}}" alt="casadecidade">
                 <br>
                 <a href="#">Saiba Mais</a>
-                <div class="col-12 primery-bg-color" id="mini-banner-3">
-                    <h2 class="primery-bg-color">Casa na Cidade</h2>
-                    <img class="img-fluid rounded" src="{{ storage('images/casa_de_cidade.jpg')}}" alt="casadecidade">
-                    <br>
-                    <a href="#">Saiba Mais</a>
-                    <br>
-                    <img class="img-fluid rounded" src="{{ storage('images/casadecidad2.jpg')}}" alt="casadecidade">
-                    <br>
-                    <a href="#">Saiba Mais</a>
-                    <br>
-                    <img class="img-fluid rounded" src="{{ storage('images/casadecidad3.jpg')}}" alt="casadecidade">
-                    <br>
-                    <a href="#">Saiba Mais</a>
-                    <br>
-                    <img class="img-fluid rounded" src="{{ storage('images/casadecid4.jpg')}}" alt="casadecidade">
-                    <br>
-                    <a href="#">Saiba Mais</a>
-                    <br>
-                </div>
+                <br>
             </div>
         </div>
     </div>
+</div>
 </div>
 <p>Como foi pensado a Web Imóveis, em um unico lugar, você tem as opções de imóveis anunciados em diversas
     plataformas de venda, assim como o seu anuncio também.
